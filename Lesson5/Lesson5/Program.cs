f5f13bfd188af3eae325b612273374b252c3589c
EgorRosliakov <egorka.030890@gmail.com>
2023-09-11 09:12:24 +0300

    Lesson5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Людина загадує число, компьютер відгадує*/
+
+Console.WriteLine("Hello, enter a number from 1 to 5 ");
+
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 1 || userNumber > 5)
+{
+    Console.WriteLine("Write the number correctly");
+    return;
+}
+while (true) 
+{
+    int randomNumber = new Random().Next(1, 6);
+    if (userNumber == randomNumber)
+    {
+        Console.WriteLine($"I guessed this number {userNumber}");
+        break;
+    } else { 
+        Console.WriteLine($"I guessed wrong my number {randomNumber} you number {userNumber}, let's try again [y/n]");
+        string text = Console.ReadLine();
+        if (text == "y")
+        {
+            continue;
+        } else
+            Console.WriteLine("Ok,Good bye");
+        break;
+    }
+}
+
+
+    
+   
+
+
+
+
+

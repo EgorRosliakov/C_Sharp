47592f833d35889a7214e9cab6fb8020328a29d5
EgorRosliakov <egorka.030890@gmail.com>
2023-09-14 12:59:11 +0300

    Lesson6

---
+++ Lesson6/Lesson6/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Реалізувати алгоритм сортування:
+ - Бульбашка*/
+
+int[] bubls = { 3, -6, 5, 26, 7, 24, 19, -20, 10, 14 };
+
+int temp;
+
+for (int i = 0; i < bubls.Length - 1; i++)
+{
+
+    for (int j = i + 1; j < bubls.Length; j++)
+    {
+
+        if (bubls[i] > bubls[j])
+        {
+            temp = bubls[i];
+            bubls[i] = bubls[j];
+            bubls[j] = temp;
+        }
+    }
+}
+
+Console.WriteLine("Outputting a sorted array");
+
+for (int i = 0;i < bubls.Length; i++)
+{
+    Console.WriteLine(bubls[i]);
+}

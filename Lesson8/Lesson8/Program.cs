341ec677a7db95440faa6c49188edd4fa69c139d
EgorRosliakov <egorka.030890@gmail.com>
2023-09-18 21:06:31 +0300

    Lesson8

---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Programm
+{
+    static void Main()
+    {
+        string path_to_file = @"C:\Users\Yehor\Desktop\privitSvit.txt";
+
+        string text = File.ReadAllText(path_to_file);
+        Console.WriteLine(text);
+
+        Console.WriteLine("How is your name: ");
+            text += Console.ReadLine();
+
+        File.WriteAllText(path_to_file, text);
+    }
+}
